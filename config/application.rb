3e05a368b78344292d5418d2784e5cfc0d7093e5
Martin Chapcak <chapcak.m@gmail.com>
2015-12-05 14:35:05 +0900

    REST api for User + CORS support

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,10 @@ module Chat
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*',
+          :headers => :any,
+          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          :methods => [:get, :post, :options, :delete]
       end
     end
   end
